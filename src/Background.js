33f47dcbdcb600fd482194fa32338017ffe6a39a
larissa <larissafranco@utexas.edu>
2024-02-03 14:21:54 -0600

    added scroll bar

--- src/Background.js
+++ src/Background.js
@@ -1,19 +1,29 @@
 import Navbar from './components/Navbar';
 function Background() {
+    // Function to scroll down
+    const scrollDown = () => {
+      window.scrollBy({
+        top: window.innerHeight, // Scroll down by the height of the viewport
+        behavior: 'smooth' // Smooth scrolling behavior
+      });
+    };
+  
     return (
       <>
-        <Navbar/>
+        <Navbar />
         <div className="background">
           <div className="background-content">
             <h1>Jeonghan's wife</h1>
-            <h3>
-              Larissa Franco is the wife of Yoon Jeonghan
-            </h3>
+            <h3>Larissa Franco is the wife of Yoon Jeonghan</h3>
+            <div className="scroll-down-btn-container">
+              <button className="scroll-down-btn" onClick={scrollDown}>
+                Scroll Down
+              </button>
+            </div>
           </div>
         </div>
-        </>
-        );
-}
-
-export default Background;
+      </>
+    );
+  }
   
+  export default Background;
