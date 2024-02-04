d847b5093dc27c821e54369194131540be84b8a9
sakura-lxver <aleanadhiraa@gmail.com>
2024-02-03 22:47:11 -0600

    soobin

---
+++ src/components/Soobin.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/Jeonghan.css';
+function Soobin() {
+    return( 
+        <div className="Soobin">
+        <div className="Soobin-background">
+          <div className="background-content">
+            <h1>I LOVE SOOBIN</h1>
+            <h3>-Alea</h3>
+            <div className="scroll-down-btn-container">
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Soobin;
