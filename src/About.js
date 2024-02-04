328f9d9c9363dc49d52b9598cdf7d1f7bf2b96da
sakura-lxver <aleanadhiraa@gmail.com>
2024-02-04 08:51:08 -0600

    finished about page

--- src/About.js
+++ src/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Alea from './19260.jpg'
 import Isha from './IMG_0007.png'
 import Sauda from './sauda pic.jpg'
+import Larissa from './IMG_2115.jpg'
+import './css/Home.css'
 function About() {
     return(
+        <div className='about-background'>
         <div style={{textAlign: 'center'}}>
             <h1 style={{fontWeight: 'bolder', fontSize: '2.5rem'}}>MEET THE TEAM</h1>
         <div class="container mt-4">
@@ -34,13 +37,14 @@ function About() {
                 </div>
                 <div class="col-md-4 mb-4">
                     <div class="card">
-                        <img src={Isha} class="card-image img-fluid" alt='Alea' />
+                        <img src={Larissa} class="card-image img-fluid" alt='Alea' />
                         <div class="card-body">
-                            <p class="card-text">Isha Cheema</p>
+                            <p class="card-text">Larissa Franco</p>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
         </div>
     )
